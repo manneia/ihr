ffd977d475216d8bb3eea13697b584d4144b500a
luo <lkx3091705416@126.com>
2022-08-11 19:37:50 +0800

    第三天完成

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { Message } from 'element-ui'
+import store from '@/store'
 
 // create an axios instance 单例模式
 const service = axios.create({
@@ -9,7 +10,16 @@ const service = axios.create({
 })
 
 // request interceptor
-service.interceptors.request.use()
+service.interceptors.request.use(config => {
+  // 统一的去注入token
+  if (store.getters.token) {
+    // 如果token存在 注入token
+    config.headers['Authorization'] = `Bearer ${store.getters.token}`
+  }
+  return config // 必须返回配置
+}, error => {
+  return Promise.reject(error)
+})
 
 // response interceptor
 // 两个参数都写上
